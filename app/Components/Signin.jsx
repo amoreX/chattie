e66b44d983ecb927314cefe97cdc4c8be11e3a45
WahidX <wahidx93@gmail.com>
2024-03-15 12:11:24 +0530

    lasdfdasfas

--- app/Components/Signin.jsx
+++ app/Components/Signin.jsx
@@ -1,9 +1,26 @@
-import "./Styles/Signin.scss"
+"use client"
 
+import "./Styles/Signin.scss"
+import {useState} from "react"
 export default function Signin(){
+    const [coords,setCoords]=useState([]);
+
+    const handlemouse=(e)=>{
+        setCoords([e.clientX,e.clientY]);
+        console.log(coords);
+    }
     return(
-        <div>
-            nuhuh
+        <div id="container" onMouseMove={(e)=>handlemouse(e)}> 
+            
+                <div id="front">
+                GOOGLE
+                </div>
+                <div id="behind"
+                style={{ transform: `translate(${coords[0] - 750}px,${coords[1] - 350}px)` }}
+                >
+
+                </div>
+            
         </div>
     )
 }
